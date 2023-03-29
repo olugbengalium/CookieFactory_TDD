aabc5561a31b6d068c7acac48ab9162c41d94071
Akinbande Gbenga <olugbengalium@gmail.com>
2023-03-29 09:27:39 +0100

    Initial Commit

---
+++ CookieFactory_TDD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CookieFactory_TDD
+//
+//  Created by Gbenga Abayomi on 29/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CookieFactory_TDDUITests/CookieFactory_TDDUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CookieFactory_TDDUITestsLaunchTests.swift
+//  CookieFactory_TDDUITests
+//
+//  Created by Gbenga Abayomi on 29/03/2023.
+//
+
+import XCTest
+
+final class CookieFactory_TDDUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
